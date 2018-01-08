4a716220d7b0a1e31ce541f578ab23f91ab623a7
brackebusch <kyle.brackebusch@gmail.com>
2018-01-07 23:47:03 -0800

    add spec and flush out music app

--- w4d5/music_app/app/models/band.rb
+++ w4d5/music_app/app/models/band.rb
@@ -1,3 +1,6 @@
 class Band < ApplicationRecord
     validates :name, uniqueness: true, presence: true
+
+    has_many :albums, 
+    dependent: :destroy
 end
--- w4d5/music_app/app/models/track.rb
+++ w4d5/music_app/app/models/track.rb
@@ -1,2 +1,20 @@
 class Track < ApplicationRecord
+    belongs_to :album
+
+    has_one :band,
+        through: :album,
+        source: :band
+
+    has_many :notes,
+        dependent: :destroy
+
+    validates :lyrics, :name, :ord, presence: true
+    validates :bonus, inclusion: {in:[true,false]}
+    validates :ord, uniqueness: {scope: :album_id}
+
+    after_initalize :set_defaults
+
+    def set_defaults
+        self.bonus ||= false
+    end
 end
